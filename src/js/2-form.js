c63527617267bf4a5ddb42c7576b1b066fabdc2d
Dictator <dictatorbogdan@gmail.com>
2026-02-01 13:22:51 +0200

    fix js-hw9 2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,18 +2,18 @@ const KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
 
-const formData = { email: '', message: '' };
+const formData = JSON.parse(localStorage.getItem(KEY)) ?? {
+  email: '',
+  message: '',
+};
 
-const localeData = JSON.parse(localStorage.getItem(KEY)) ?? {};
-localStorage.setItem(KEY, JSON.stringify(formData));
+const { message, email } = formData;
 
-const { message, email } = localeData;
+form.elements.message.value = message;
+form.elements.email.value = email;
 
-form.elements.message.value = message ?? '';
-form.elements.email.value = email ?? '';
-
-formData.email = email ?? '';
-formData.message = message ?? '';
+formData.email = email;
+formData.message = message;
 
 form.addEventListener('input', inputClick);
 form.addEventListener('submit', formSubmit);
@@ -21,6 +21,7 @@ form.addEventListener('submit', formSubmit);
 function inputClick(evt) {
   const { target } = evt;
   formData[target.name] = target.value.trim();
+  localStorage.setItem(KEY, JSON.stringify(formData));
 }
 
 function formSubmit(evt) {
@@ -29,8 +30,10 @@ function formSubmit(evt) {
   if (formData.email && formData.message) {
     console.log(formData);
     localStorage.removeItem(KEY);
-    form.reset();
+    evt.target.reset();
+    formData.email = '';
+    formData.message = '';
   } else {
-    console.log('Fill please all fields');
+    alert('Fill please all fields');
   }
 }
