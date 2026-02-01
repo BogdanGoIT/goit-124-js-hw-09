dcfe70c66f76cc91ce6767afc74a0b4fe90bb341
Dictator <dictatorbogdan@gmail.com>
2026-02-01 12:50:30 +0200

    fix localeStorage

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,12 +1,11 @@
 const KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input', inputClick);
-form.addEventListener('submit', formSubmit);
 
 const formData = { email: '', message: '' };
 
 const localeData = JSON.parse(localStorage.getItem(KEY)) ?? {};
+localStorage.setItem(KEY, JSON.stringify(formData));
 
 const { message, email } = localeData;
 
@@ -16,11 +15,12 @@ form.elements.email.value = email ?? '';
 formData.email = email ?? '';
 formData.message = message ?? '';
 
+form.addEventListener('input', inputClick);
+form.addEventListener('submit', formSubmit);
+
 function inputClick(evt) {
   const { target } = evt;
   formData[target.name] = target.value.trim();
-
-  localStorage.setItem(KEY, JSON.stringify(formData));
 }
 
 function formSubmit(evt) {
@@ -30,8 +30,6 @@ function formSubmit(evt) {
     console.log(formData);
     localStorage.removeItem(KEY);
     form.reset();
-    formData.email = '';
-    formData.message = '';
   } else {
     console.log('Fill please all fields');
   }
